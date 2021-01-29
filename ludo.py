d7e5c02b2f2ff12c01d303450f459d7d020acfef
unknown <prokopova.kristyna@email.cz>
2021-01-29 16:05:52 +0100

    setting yards no control

--- ludo.py
+++ ludo.py
@@ -1,10 +1,46 @@
 import random
+
+class Pawn(): #TODO
+    def __init__(self ,board):
+        self.positionColumn
+        self.positionRow
+        self.color
+    def move(self): #TODO
+        pass
+    def remove(self): #TODO
+        pass
 class Game:
     def __init__(self):
+        self.sizeBoard = 17
+        self.board = [[0] for i in range(self.sizeBoard)]
         self.turn = 1
         self.numberOfPlayers = 1
         pass
+   #sets yard on the board
+    def setYards(self):
+
+         #set a yard of size 7x7 filled with color of a player on certain position 
+        def setYard(startUpRow, startLeftColumn, filling):
+            sizeofYard = 7
+
+            assert sizeofYard + startLeftColumn < self.sizeBoard and sizeofYard + startUpRow < self.sizeBoard, 'Yard is out of board'
+            for i in range(sizeofYard):
+                
+                self.board[startUpRow][startLeftColumn + i] = filling #up boundary
+                self.board[startUpRow + sizeofYard - 1][startLeftColumn + i] = filling  #down boundary
+                self.board[startUpRow + i][startLeftColumn] = filling    #left boundary
+                self.board[startUpRow + i][startLeftColumn + sizeofYard - 1] = filling    #rightboundary
+            
+            # colors places for pawns
+            self.board[startUpRow + 2][startLeftColumn + 2] = filling
+            self.board[startUpRow + 2][startLeftColumn + 4] = filling
+            self.board[startUpRow + 4][startLeftColumn + 2] = filling
+            self.board[startUpRow + 4][startLeftColumn + 4] = filling
+
+    #set yard of players
+
     def setNumberOfPlayers(self):
+        pass
 
     def newPawn(self):  #TODO
         print('new pawn')
