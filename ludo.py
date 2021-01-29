9b84316ef790b71dee23e676d9068a16986de94b
unknown <prokopova.kristyna@email.cz>
2021-01-29 12:57:45 +0100

    dice working

--- ludo.py
+++ ludo.py
@@ -1,18 +1,32 @@
 import random
 class Game:
     def __init__(self):
+        self.turn = 1
+        self.numberOfPlayers = 1
         pass
+    def setNumberOfPlayers(self):
+
+    def newPawn(self):  #TODO
+        print('new pawn')
+
+    def movingPawn(self): #TODO
+        print('moving pawn')
 
 
 
+    #switch to the next Player
+    def switchPlayer(self):
+        self.turn = (self.turn + 1) % (self.numberOfPlayers) + 1
 
     def rollingDice(self):
         #print message of options you have when you roll 6
         def printOptionsMessage():
-            print() #TODO
+            print('TODO message for choice') #TODO
+       
         #rolling a dice
-        #message TODO?
+        print('Rolling a dice ...')
         numberRolled = random.randint(1, 6)
+        print('Number rolled is:', numberRolled)
 
         if numberRolled == 6:   #if 6 is rolled
             printOptionsMessage() 
@@ -23,10 +37,16 @@ class Game:
                 choice = input()
             
             if choice == 'new':
-                newPawn()   #TODO
+                self.newPawn()   #TODO
             if choice == 'move':
-                movingPawn()    #TODO
+                self.movingPawn()    #TODO
 
-    def printPlayer()
+    def printPlayer(self):   #TODO
+        pass
+
+    
 
 
+game = Game()
+while(True):
+    game.rollingDice()
